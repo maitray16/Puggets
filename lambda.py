b3f568d6912fb89458d827cbcfebde585dd1a6f2
Maitray Shah <maitrayshah16@gmail.com>
2019-01-15 18:54:18 -0800

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,1 +1,14 @@
 
+#lambda
+def add(x, y): 
+    return x + y
+print(add(1, 2))
+
+add = lambda x, y : x + y 
+print(add(1, 2))
+
+#map
+def multiply(x):
+  return x * 2
+print(list(map(multiply, [1, 2, 3, 4])))
+
